497151837e29871c80bf1d80a2e63d230e1dcad5
Egor <escaton@yandex-team.ru>
2013-05-29 20:06:06 +0400

    выводится собранная инфа по библиотекам и статус

--- tools/get-libraries.js
+++ tools/get-libraries.js
@@ -1,28 +1,132 @@
-var Vow = require('vow');
-var exec = require("child_process").exec;
-var Path = require("path");
-var fs = require("fs");
+//TODO
+/*
+    Проверять наличие папки для чекаута
+    Смотреть в process.argv[]
+*/
 
-var mergeLibraries = function(oldConfig, newConfig) {
+var Vow = require('vow'),
+    exec = require("child_process").exec,
+    Path = require("path"),
+    fs = require("fs");
+    // inherit = require('inherit');
 
-    var config = oldConfig;
-    var newLibs = Object.keys(newConfig);
+var api = {
 
-    newLibs.forEach(function(lib) {
+    _libraries: {},
 
-        if (config.hasOwnProperty(lib)) {
+    addLibraries: function(newLibraries) {
 
-            var props = Object.keys(newConfig[lib]);
-            props.forEach(function(prop) {
+        var libs = this._libraries;
+        var newLibs = Object.keys(newLibraries);
 
-                config[lib][prop] = newConfig[lib][prop];
-            })
+        newLibs.forEach(function(lib) {
+
+            if (libs.hasOwnProperty(lib)) {
+
+                var props = Object.keys(newLibraries[lib]);
+                props.forEach(function(prop) {
+
+                    libs[lib][prop] = newLibraries[lib][prop];
+                })
+
+            } else {
+                libs[lib] = newLibraries[lib];
+            }
+        })
+        return this;
+    },
+
+    checkoutLibraries: function(enbTask) {
+
+        var promise;
+        var libs = this._libraries;
+        var libNames = Object.keys(libs);
+        var _this = this;
+
+        promise = Vow.all(libNames.map(function(libName) {
+
+            var lib = libs[libName];
+            lib.treeish = lib.treeish || "master";
+
+            return _this.getLibState(lib, libName)
+                .then(function(state) {
+                    enbTask.log(state + ' ' + libName);
+                    console.log(lib);
+                    // if (state !== "exist") {
+                    //     task.shell(commnads[state]);
+                    // } else {
+
+                    // }
+                },
+                function(error) {
+                    task.log(error + ' ' + libName)
+                })
+        }))
+
+        return promise;
+    },
+
+    getLibState: function(lib, path) {
+
+        var promise = Vow.promise();
+        var state = "doesn't exist";
+        if (fs.existsSync(Path.resolve(path))) {
+            state = "exist";
+
+            this.getLibRepositoryInfo(path, lib.url)
+                .then(function(info) {
+                    if (info.indexOf(lib.treeish) === -1) {
+                        state = "another branch";
+                    } else if (typeof info === 'string') {
+                        state = info;
+                    }
+                    promise.fulfill(state)
+                },
+                function(error) {
+                    promise.reject(error);
+                })
 
         } else {
-            config[lib] = newConfig[lib];
+            promise.fulfill(state)
         }
-    })
-    return config;
+        return promise;
+    },
+
+    getLibRepositoryInfo: function(path, url) {
+        var promise = Vow.promise();
+        var repositoryInfo = [];
+        var cd = "cd "+path+" && ";
+
+        promiseExec(cd + "git config --get remote.origin.url")
+            .then(function(stdout) {
+                var remote = stdout.split("\n")[0];
+                console.log(remote)
+                if (remote !== url) {
+                    promise.fulfill('another repository');
+                    return;
+                }
+                promiseExec(cd + "git rev-parse HEAD")
+                    .then(function(stdout) {
+                        var hash = stdout.split("\n")[0];
+                        repositoryInfo.push(hash);
+                        return promiseExec(cd + "git describe --always --tag");
+                    })
+                    .then(function(stdout) {
+                        var tag = stdout.split("\n")[0];
+                        repositoryInfo.push(tag);
+                        return promiseExec(cd + "git rev-parse --abbrev-ref HEAD");
+                    })
+                    .then(function(stdout) {
+                        var branch = stdout.split("\n")[0];
+                        repositoryInfo.push(branch);
+                        promise.fulfill(repositoryInfo)
+                    });
+            },
+            function(error) {
+                promise.reject(error)
+            })
+        return promise;
+    }
 }
 
 var promiseExec = function(cmd) {
@@ -41,112 +145,12 @@ var promiseExec = function(cmd) {
     return promise;
 }
 
-var getLibRepositoryInfo = function(path, url) {
-    var promise = Vow.promise();
-    var repositoryInfo = [];
-    var cd = "cd "+path+" && ";
+// var commnads = {
+//     "doesn't exist": "git clone {lib.url} libName && cd {lib.Name} && git checkout {lib.treeish}",
+//     "another branch": "cd {lib.Name} && git checkout {lib.treeish}"
+// };
 
-    promiseExec(cd + "git config --get remote.origin.url")
-        .then(function(stdout) {
-            var remote = stdout.split("\n")[0];
-            console.log(remote)
-            if (remote !== url) {
-                promise.fulfill('another repository');
-                return;
-            }
-            promiseExec(cd + "git rev-parse HEAD")
-                .then(function(stdout) {
-                    var hash = stdout.split("\n")[0];
-                    repositoryInfo.push(hash);
-                    return promiseExec(cd + "git describe --always --tag");
-                })
-                .then(function(stdout) {
-                    var tag = stdout.split("\n")[0];
-                    repositoryInfo.push(tag);
-                    return promiseExec(cd + "git rev-parse --abbrev-ref HEAD");
-                })
-                .then(function(stdout) {
-                    var branch = stdout.split("\n")[0];
-                    repositoryInfo.push(branch);
-                    promise.fulfill(repositoryInfo)
-                });
-        },
-        function(error) {
-            promise.reject(error)
-        })
-    return promise;
-}
-
-var getLibState = function(lib, path) {
-
-    var promise = Vow.promise();
-    var state = "doesn't exist";
-    if (fs.existsSync(Path.resolve(path))) {
-        state = "exist";
-
-        getLibRepositoryInfo(path, lib.url)
-            .then(function(info) {
-                if (info.indexOf(lib.treeish) === -1) {
-                    state = "another branch";
-                } else if (typeof info === 'string') {
-                    state = info;
-                }
-                promise.fulfill(state)
-            },
-            function(error) {
-                promise.reject(error);
-            })
-
-    } else {
-        promise.fulfill(state)
-    }
-    return promise;
-}
-
-var commnads = {
-    "doesn't exist": "git clone {lib.url} libName && cd {lib.Name} && git checkout {lib.treeish}",
-    "another branch": "cd {lib.Name} && git checkout {lib.treeish}"
-};
-
-var num;
-
-var api = {
-    setNumber: function(i) {
-        num = i;
-    },
-    getLibraries: function() {
-        console.log(num);
-    }
-}
 
 module.exports = function(config) {
     return config.registerModule('enb-checkout', api);
 }
-
-// module.exports = function(task, config) {
-//     var promise;
-//     var libs = mergeLibraries(config.libraries || {}, config.personalLibraries || {});
-//     var libNames = Object.keys(libs);
-//     var lib;
-
-//     promise = Vow.all(libNames.map(function(libName) {
-
-//         lib = libs[libName];
-//         lib.treeish = lib.treeish || "master";
-
-//         return getLibState(lib, libName)
-//             .then(function(state) {
-//                 task.log(state + ' ' + libName)
-//                 // if (state !== "exist") {
-//                 //     task.shell(commnads[state]);
-//                 // } else {
-
-//                 // }
-//             },
-//             function(error) {
-//                 task.log(error + ' ' + libName)
-//             })
-//     }))
-
-//     return promise;
-// }
